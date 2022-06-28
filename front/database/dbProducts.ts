66ea332ab611197c9fd1d4129425d16dc886857a
Cristian Carmona <cristiancarmona44@gmail.com>
2022-06-28 12:58:34 -0500

    prueba bus

--- front/database/dbProducts.ts
+++ front/database/dbProducts.ts
@@ -27,10 +27,27 @@ export const getAllProductSlugs = async (): Promise<ProductSlug[]> => {
   return slugs;
 };
 
-export const getProductsByTerm = async (term: string): Promise<IProduct[]> => {
+export const getProductsByTerm = (term: string): any[] => {
   term = term.toString().toLowerCase();
   
-    let response = await fetch('https://globalmarkets.herokuapp.com/products').then(res=>res.json());
+    /*let response = await fetch('https://globalmarkets.herokuapp.com/products').then(res=>res.json());*/
+    let response = [{_id: "62b3eada7a13eb8783c71b0e",
+    title: "Control Inalámbrico DualShock 4 - Midnight Blue - PlayStation 4",
+    description: "Mejor precisión de Control: el tacto, la forma, y la sensibilidad de los sticks analógicos del DualShock4 y botones de disparo se han mejorado. El nuevo botón de Compartir (Share) permite ver y compartir videos en tiempo real con un solo clic",
+    images: [
+        "https://m.media-amazon.com/images/I/51--ljrR+wL._AC_SX679_.jpg",
+        "https://m.media-amazon.com/images/I/51AOgSqSbOL._AC_SX679_.jpg"
+    ],
+    inStock: 23,
+    price: 160000,
+    sizes: [],
+    slug: "controles",
+    tags: "playstation,mando, gamer",
+    caterogiras: [
+        "tecnologia"
+    ],
+    type: "controles",
+    gender: "videojuegos",}];
     let products = response.filter((i:any)=>{
         let tag = Array.isArray(i.tags)? i.tags[0] : i.tags
         if(i.title.toLowerCase().includes(term)) return i;
